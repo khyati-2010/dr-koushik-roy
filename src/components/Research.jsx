b02a247cba54d462462e2733620178428e00415d
Khyati Aggarwal <agg.khyati2007@gmail.com>
2026-06-17 18:11:06 +0530

    research updated

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -5,18 +5,19 @@ function Research() {
             <div className="container">
                 <h2>Research & Students Supervised</h2>
                 <div className="research-tags">
-                    {faculty.researchAreas.map((area, index) => {
+                    {faculty.researchAreas.map((area, index) => (
                         <span key={index} className="research-tag">
                             {area}
                         </span>
-                    })}
+                    )    
+                    )}
                 </div>
                 <div className="students-block">
                     <h3>Current Students</h3>
                     {faculty.students.currentStud.length > 0 ? (
                         <div className="student-list">
                             {faculty.students.currentStud.map((student, index) => {
-                                return (<div className="student-card">
+                                return (<div className="student-card" key={index}>
                                     <h4>{student.name}</h4>
                                     <p>{student.course}</p>
                                     <p>{student.resArea}</p>
@@ -24,7 +25,7 @@ function Research() {
                             })}
                         </div>
                     ):(
-                        <p>No information available.</p>
+                        <p>Data will be updated from /data/facultyData.js</p>
                     )}
                 </div>
                 <div className="students-block">
@@ -32,7 +33,7 @@ function Research() {
                     {faculty.students.alumni.length > 0 ? (
                         <div className="student-list">
                             {faculty.students.alumni.map((student, index) => {
-                                return (<div className="student-card">
+                                return (<div className="student-card" key={index}>
                                     <h4>{student.name}</h4>
                                     <p>{student.course}</p>
                                     <p>{student.resArea}</p>
@@ -40,7 +41,7 @@ function Research() {
                             })}
                         </div>
                     ):(
-                        <p>No information available.</p>
+                        <p>Data will be updated from /data/facultyData.js</p>
                     )}
                 </div>
             </div>
